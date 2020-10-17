23c1b57438350fab5cdeda2dbd67c72fcebf1a18
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-10-17 17:43:39 -0500

    update/  Auth and home modules and components are generated, shared module is created with header and footer components, and sass variables are generated and layout component is created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        loadChildren: () => import ('./home/home.module').then(m => m.HomeModule)
+      },
+  ]
+  },
+  {
+    path: '',
+    loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules, useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
