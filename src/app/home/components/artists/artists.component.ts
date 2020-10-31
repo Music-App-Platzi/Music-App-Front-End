c29e10faff340e7019ee42e922bdf1b0fe3352d1
Ktagudelo <47460810+Ktagudelo@users.noreply.github.com>
2020-10-30 19:57:53 -0500

    Fix / Carousel home, albums and artists

--- src/app/home/components/artists/artists.component.ts
+++ src/app/home/components/artists/artists.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArtistsComponent implements OnInit {
 
+  imagesArtist: string[] = [
+    'assets/img__carrousel/kamelia.jpg',
+    'assets/img__carrousel/faydee.jpg',
+    'assets/img__carrousel/carly.jpg',
+    'assets/img__carrousel/jonasBrothers.jpg',
+    'assets/img__carrousel/bilionera.jpg',
+    'assets/img__carrousel/edSheeran.jpg',
+    'assets/img__carrousel/lilNas.jpg',
+    'assets/img__carrousel/drake.jpg',
+    'assets/img__carrousel/faydeedj.jpg',
+    'assets/img__carrousel/carly.jpg',
+    'assets/img__carrousel/faydee.jpg',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
