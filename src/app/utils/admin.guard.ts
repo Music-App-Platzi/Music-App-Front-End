0c70d12160d47ca68f5c28b45887156a02df0333
Johan Galeano <johans.galeanot@hotmail.com>
2020-11-01 23:15:56 -0500

    fix/ admin guard

--- src/app/utils/admin.guard.ts
+++ src/app/utils/admin.guard.ts
@@ -10,17 +10,17 @@ import { Router } from '@angular/router';
 export class AdminGuard implements CanActivate {
   constructor(
     private router: Router
-  ){}
+  ) { }
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const user: any = JSON.parse(sessionStorage.getItem('user'));
-      const rolId = user.user.rol_id;
-      if ( rolId === 1){
-        return true;
-      }else {
-        this.router.navigate(['/login']);
-        return false;
-      }
+    const user: any = JSON.parse(sessionStorage.getItem('user'));
+    const rolId = user.user.rol_id;
+    if (rolId === 1) {
+      return true;
+    } else {
+      this.router.navigate(['/home']);
+      return false;
+    }
   }
 }
